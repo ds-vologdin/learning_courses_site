c75d44fc80fd652dbba52d2a99672f38e5015e93
Dmitry Vologdin <ds.vologdin@yandex.ru>
2018-07-02 19:10:11 +0300

    Отказался от конфига в /etc/ в пользу django-configurations

--- web_learning_course/settings.py
+++ web_learning_course/settings.py
@@ -1,72 +1,61 @@
+from configurations import Configuration
 import os
-import logging
 
-from web_learning_course.parse_config import parse_config
+from .settings_privat import DatabaseDevMixins, SecretKeyMixins
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9#5qjd$7evu5zhyucw-_=80e(yjqid^hgzrf0g@3$k)7n%hke$'
+class Base(Configuration):
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-DEBUG = True
+    # Обязательно переоределяйте SECRET_KEY с помощью SecretKeyMixins
+    SECRET_KEY = '9#5qjd$7evu5zhyucw-_=80e(yjqid^hgzrf0g@3$k)7n%hke$'
 
-ALLOWED_HOSTS = []
+    DEBUG = False
 
-INSTALLED_APPS = [
-    'courses.apps.CoursesConfig',
-    'teachers.apps.TeachersConfig',
-    'students.apps.StudentsConfig',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-]
+    ALLOWED_HOSTS = []
 
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
+    INSTALLED_APPS = [
+        'courses.apps.CoursesConfig',
+        'teachers.apps.TeachersConfig',
+        'students.apps.StudentsConfig',
+        'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
+    ]
 
-ROOT_URLCONF = 'web_learning_course.urls'
+    MIDDLEWARE = [
+        'django.middleware.security.SecurityMiddleware',
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    ]
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
+    ROOT_URLCONF = 'web_learning_course.urls'
+
+    TEMPLATES = [
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'DIRS': [],
+            'APP_DIRS': True,
+            'OPTIONS': {
+                'context_processors': [
+                    'django.template.context_processors.debug',
+                    'django.template.context_processors.request',
+                    'django.contrib.auth.context_processors.auth',
+                    'django.contrib.messages.context_processors.messages',
+                ],
+            },
         },
-    },
-]
+    ]
 
-WSGI_APPLICATION = 'web_learning_course.wsgi.application'
+    WSGI_APPLICATION = 'web_learning_course.wsgi.application'
 
-
-# Database
-file_config = '/etc/django_web_learning_course.conf'
-config = parse_config(file_config)
-if not config:
-    # если /etc/django_market.conf не смогли прочитать, берём конфиг из примера
-    logging.error('config file "{}" not found'.format(file_config))
-    config = parse_config('market/django_web_learning_course.conf')
-
-if 'DATABASES' in config:
-    DATABASES = config['DATABASES']
-else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
@@ -74,28 +63,32 @@ else:
         }
     }
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
+    AUTH_PASSWORD_VALIDATORS = [
+        {
+            'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        },
+    ]
 
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
-USE_I18N = True
-USE_L10N = True
-USE_TZ = True
+    LANGUAGE_CODE = 'en-us'
+    TIME_ZONE = 'UTC'
+    USE_I18N = True
+    USE_L10N = True
+    USE_TZ = True
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR + '/static/'
-MEDIA_ROOT = BASE_DIR + '/media/'
-MEDIA_URL = '/media/'
+    STATIC_URL = '/static/'
+    STATIC_ROOT = BASE_DIR + '/static/'
+    MEDIA_ROOT = BASE_DIR + '/media/'
+    MEDIA_URL = '/media/'
+
+
+class Dev(DatabaseDevMixins, SecretKeyMixins, Base):
+    DEBUG = True
--- web_learning_course/wsgi.py
+++ web_learning_course/wsgi.py
@@ -1,16 +1,9 @@
-"""
-WSGI config for web_learning_course project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
-"""
-
 import os
 
-from django.core.wsgi import get_wsgi_application
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web_learning_course.settings')
+os.environ.setdefault('DJANGO_CONFIGURATION', 'Dev')
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web_learning_course.settings")
+from configurations.wsgi import get_wsgi_application
+
 
 application = get_wsgi_application()
