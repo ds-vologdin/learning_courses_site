6100b14728e11262c74c351cc170f5e45a903581
Dmitry Vologdin <ds.vologdin@yandex.ru>
2018-07-02 15:01:25 +0300

    Немного bootstrap 4.1

--- students/forms.py
+++ students/forms.py
@@ -5,19 +5,33 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import ProfileStudent, Profile
 
 
+class CSSClassInputRegisterMixins():
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({'class': 'input-register'})
+
+
 class UserForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({'class': 'input-register'})
+
     class Meta:
         model = User
         fields = ['username', 'email']
 
 
-class UserProfileForm(ModelForm):
+class UserProfileForm(CSSClassInputRegisterMixins, ModelForm):
+
     class Meta:
         model = Profile
         exclude = ['user']
 
 
-class UserProfileStudentForm(ModelForm):
+class UserProfileStudentForm(CSSClassInputRegisterMixins, ModelForm):
+
     class Meta:
         model = ProfileStudent
         exclude = ['user']
--- web_learning_course/urls.py
+++ web_learning_course/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.staticfiles.urls import static
-# from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from . import settings
 
@@ -12,5 +11,7 @@ urlpatterns = [
     path('courses/', include('courses.urls')),
 ]
 
-# urlpatterns += staticfiles_urlpatterns()
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
