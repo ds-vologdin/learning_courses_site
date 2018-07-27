63e56573f0176a0147cfdf5c7cf0593d99221885
Dmitry Vologdin <ds.vologdin@yandex.ru>
2018-07-27 16:01:05 +0300

    Запрос списка курсов и описания курсов

--- html/webpack/src/courses.js
+++ html/webpack/src/courses.js
@@ -1,16 +1,23 @@
+const HOST = 'http://127.0.0.1:8000/'
+
 export let fetch_courses = () => {
-  fetch(
-    'http://127.0.0.1:8000/courses/api/courses/'
-  ).then(
-    function(response) {
-      console.log(response);
-      if (response.ok) {
-        return response.json();
-      }
+  Promise.all([
+    fetch(HOST + 'courses/api/coursedescription/'),
+    fetch(HOST + 'courses/api/courses/'),
+  ]).then(allResponses => {
+    const response_course_description = allResponses[0]
+    const response_course = allResponses[1]
+    if (!response_course_description.ok || !response_course.ok) {
       throw new Error('Network response was not ok.');
     }
-  ).then(function(curses_json) {
-      console.log(curses_json);
+    let course_description = response_course_description.json()
+    let course = response_course.json()
+    return {
+      'course_description': course_description,
+      'course': course
+    }
+  }).then(function(curses) {
+      console.log(curses);
   }).catch(function(error) {
     console.log('There has been a problem with your fetch operation: ' + error.message);
   });
