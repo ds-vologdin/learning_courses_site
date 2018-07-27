aef7363fd1e4ddd6c46b8dfd6a287e60b5589eba
Dmitry Vologdin <ds.vologdin@yandex.ru>
2018-07-27 16:53:37 +0300

    На пути к вложенным запросам fetch

--- html/webpack/src/courses.js
+++ html/webpack/src/courses.js
@@ -1,23 +1,33 @@
 const HOST = 'http://127.0.0.1:8000/'
 
+
+let get_courses = (curses_descriptions) => {
+  console.log(curses_descriptions.course);
+  let courses = curses_descriptions.course;
+  console.log(courses.length);
+  for (let i; i < courses.length; i++) {
+    console.log(courses[i]);
+    console.log(i);
+  }
+}
+
+
 export let fetch_courses = () => {
-  Promise.all([
-    fetch(HOST + 'courses/api/coursedescription/'),
-    fetch(HOST + 'courses/api/courses/'),
-  ]).then(allResponses => {
-    const response_course_description = allResponses[0]
-    const response_course = allResponses[1]
-    if (!response_course_description.ok || !response_course.ok) {
+    fetch(HOST + 'courses/api/courses/').then(response => {
+    if (!response.ok) {
       throw new Error('Network response was not ok.');
     }
-    let course_description = response_course_description.json()
-    let course = response_course.json()
-    return {
-      'course_description': course_description,
-      'course': course
-    }
-  }).then(function(curses) {
-      console.log(curses);
+    return response.json();
+  }).then(function(courses) {
+      fetch(HOST + 'courses/api/coursedescription/').then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok.');
+        }
+        return response.json()
+      }).then(function(courses_description) {
+        console.log(courses);
+        console.log(courses_description);
+      })
   }).catch(function(error) {
     console.log('There has been a problem with your fetch operation: ' + error.message);
   });
