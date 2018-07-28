d13cecc8e979fe74d16d288546859955c3192358
Dmitry Vologdin <ds.vologdin@yandex.ru>
2018-07-28 13:27:45 +0300

    Сборка course с description в одну структуру

--- html/webpack/src/courses.js
+++ html/webpack/src/courses.js
@@ -1,17 +1,6 @@
 const HOST = 'http://127.0.0.1:8000/'
 
 
-let get_courses = (curses_descriptions) => {
-  console.log(curses_descriptions.course);
-  let courses = curses_descriptions.course;
-  console.log(courses.length);
-  for (let i; i < courses.length; i++) {
-    console.log(courses[i]);
-    console.log(i);
-  }
-}
-
-
 export let fetch_courses = () => {
     fetch(HOST + 'courses/api/courses/').then(response => {
     if (!response.ok) {
@@ -19,15 +8,23 @@ export let fetch_courses = () => {
     }
     return response.json();
   }).then(function(courses) {
-      fetch(HOST + 'courses/api/coursedescription/').then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok.');
+    fetch(HOST + 'courses/api/coursedescription/').then(response => {
+      return response.json();
+    }).then(function(courses_description) {
+      for (let course of courses) {
+        for (let description of courses_description) {
+          if (description.pk === course.course_description_id) {
+            course.description = description;
+            break;
+          }
         }
-        return response.json()
-      }).then(function(courses_description) {
-        console.log(courses);
-        console.log(courses_description);
-      })
+      }
+      return courses
+    }).then(function(courses) {
+      console.log(courses);
+    }).catch(function(error) {
+      console.log('There has been a problem with your fetch operation: ' + error.message);
+    });
   }).catch(function(error) {
     console.log('There has been a problem with your fetch operation: ' + error.message);
   });
