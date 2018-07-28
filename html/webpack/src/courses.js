9efff22003d8853cee2bc916c5106b669693d2a4
Dmitry Vologdin <ds.vologdin@yandex.ru>
2018-07-28 14:24:30 +0300

    Создаём элементы на странице

--- html/webpack/src/courses.js
+++ html/webpack/src/courses.js
@@ -22,6 +22,34 @@ export let fetch_courses = () => {
       return courses
     }).then(function(courses) {
       console.log(courses);
+      let courses_block = document.getElementsByClassName('courses-block__courses');
+      if (courses_block.length !== 1) {
+        console.log('Не нашли блок с курсами');
+        return
+      }
+      courses_block = courses_block[0]
+      for (let course of courses) {
+        let a_courses_item = document.createElement('a')
+        a_courses_item.classList.add('courses__item');
+        a_courses_item.href = '/courses/' + course.description.code_name
+
+        let div_course = document.createElement('div');
+        div_course.classList.add('course-item__block');
+        div_course.classList.add('course');
+        a_courses_item.appendChild(div_course);
+
+        let div_course_tittle = document.createElement('div');
+        div_course_tittle.classList.add('course__tittle');
+        div_course_tittle.innerHTML = course.description.name;
+        div_course.appendChild(div_course_tittle);
+
+        let div_course_date = document.createElement('div');
+        div_course_date.classList.add('course__date');
+        div_course_date.innerHTML = 'С ' + course.date_begin + ' ' + course.duration_month + ' месяцев';
+        div_course.appendChild(div_course_date);
+
+        courses_block.appendChild(a_courses_item);
+      }
     }).catch(function(error) {
       console.log('There has been a problem with your fetch operation: ' + error.message);
     });
