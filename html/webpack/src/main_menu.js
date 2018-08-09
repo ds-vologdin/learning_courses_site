fcf74c0b2c68fb0d1ba53540b03866bf950609b0
Dmitry Vologdin <ds.vologdin@yandex.ru>
2018-08-09 15:11:43 +0300

    стрелочные функции

--- html/webpack/src/main_menu.js
+++ html/webpack/src/main_menu.js
@@ -1,26 +1,26 @@
-function show_dropdown_menu() {
-  let dropdown_menu = this.getElementsByClassName('dropdown-js');
+ const show_dropdown_menu = (element) => {
+  let dropdown_menu = element.getElementsByClassName('dropdown-js');
   if (dropdown_menu.length === 0) {
     return null
   }
   dropdown_menu[0].classList.remove('dropdown-hide');
 }
 
-function hide_dropdown_menu() {
-  let dropdown_menu = this.getElementsByClassName('dropdown-js');
+const hide_dropdown_menu = (element) => {
+  let dropdown_menu = element.getElementsByClassName('dropdown-js');
   if (dropdown_menu.length === 0) {
     return null
   }
   dropdown_menu[0].classList.add('dropdown-hide');
 }
 
-export let set_handler_main_menu = () => {
+export const set_handler_main_menu = () => {
   // Навешиваем хендлеры на нажатие на заголовок урока
   let dropdown_menus = document.getElementsByClassName('dropdown-js-control');
   if (dropdown_menus.length > 0) {
     for (let dropdown_menu of dropdown_menus) {
-      dropdown_menu.addEventListener('mouseover', show_dropdown_menu.bind(dropdown_menu));
-      dropdown_menu.addEventListener('mouseout', hide_dropdown_menu.bind(dropdown_menu));
+      dropdown_menu.addEventListener('mouseover', event => show_dropdown_menu(dropdown_menu));
+      dropdown_menu.addEventListener('mouseout', event => hide_dropdown_menu(dropdown_menu));
     }
   }
 }
