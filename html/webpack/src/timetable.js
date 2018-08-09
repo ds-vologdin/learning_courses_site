1c63a8d19963a9182d6cf1c31344572e1f090a4c
Dmitry Vologdin <ds.vologdin@yandex.ru>
2018-08-09 15:24:41 +0300

    стрелочные функции timetable.js

--- html/webpack/src/timetable.js
+++ html/webpack/src/timetable.js
@@ -1,6 +1,6 @@
 const COUNT_LESSONS_IN_MONTH = 4
 
-function insert_iframe_youtube(lesson_element) {
+const insert_iframe_youtube = lesson_element => {
   if (lesson_element.getElementsByClassName('media__iframe').length > 0) {
     return false;
   }
@@ -26,13 +26,13 @@ function hide_lessons_descriptions() {
 }
 
 
-function switch_lesson_description () {
-  let description = this.getElementsByClassName('lesson__description');
+const switch_lesson_description = element => {
+  let description = element.getElementsByClassName('lesson__description');
 
   if (description[0].classList.contains('lesson__description--hidden') === true) {
     hide_lessons_descriptions();
     description[0].classList.remove('lesson__description--hidden');
-    insert_iframe_youtube(this);
+    insert_iframe_youtube(element);
   } else {
     hide_lessons_descriptions();
   }
@@ -40,7 +40,7 @@ function switch_lesson_description () {
 }
 
 
-function disable_buttons(element) {
+const disable_buttons = element => {
   let buttons = element.getElementsByClassName('buttons-month__item');
   for (let button of buttons) {
     button.classList.remove('buttons-month__item--active');
@@ -48,7 +48,7 @@ function disable_buttons(element) {
 }
 
 
-function show_month_lesson(month, count_lessons_in_month=COUNT_LESSONS_IN_MONTH) {
+const show_month_lesson = (month, count_lessons_in_month=COUNT_LESSONS_IN_MONTH) => {
   let lessons = document.getElementsByClassName('lessons__item');
   for (let lesson of lessons) {
     lesson.classList.add('lessons__item--hidden');
@@ -68,11 +68,11 @@ function show_month_lesson(month, count_lessons_in_month=COUNT_LESSONS_IN_MONTH)
 }
 
 
-function switch_month_lesson() {
-  disable_buttons(this.parentNode);
-  this.classList.add('buttons-month__item--active');
+const switch_month_lesson = element => {
+  disable_buttons(element.parentNode);
+  element.classList.add('buttons-month__item--active');
 
-  for (let class_name of this.classList) {
+  for (let class_name of element.classList) {
     if (class_name.indexOf('month-') === 0) {
       // 'month-'.length === 6
       let month = parseInt(class_name.slice(6), 10)
@@ -81,12 +81,12 @@ function switch_month_lesson() {
   }
 }
 
-export let set_handler_timetable = () => {
+export const set_handler_timetable = () => {
   // Навешиваем хендлеры на нажатие на заголовок урока
   let lesson_titles = document.getElementsByClassName('lesson-tittle__text');
   if (lesson_titles.length > 0) {
     for (let lesson_title of lesson_titles) {
-      lesson_title.addEventListener('click', switch_lesson_description.bind(lesson_title.parentNode));
+      lesson_title.addEventListener('click', event => switch_lesson_description(lesson_title.parentNode));
     }
   }
 
@@ -94,7 +94,7 @@ export let set_handler_timetable = () => {
   let buttons_month = document.getElementsByClassName('buttons-month__item');
   if (buttons_month.length > 0) {
     for (let button_month of buttons_month) {
-      button_month.addEventListener('click', switch_month_lesson.bind(button_month));
+      button_month.addEventListener('click', event => switch_month_lesson(button_month));
     }
   }
 }
