3413451105f2c26220d61a7464b8f27cf77891aa
Dmitry Vologdin <ds.vologdin@yandex.ru>
2018-08-14 13:34:52 +0300

    header from react

--- front-end/src/Header.js
+++ front-end/src/Header.js
@@ -5,10 +5,29 @@ import './less/Header.less';
 export default class Header extends Component {
   render() {
     return (
-        <header className="header">
-          <img src={logo} className="header__logo" alt="logo" />
-          <h1 className="header__title">Учиться, учиться и ещё раз учиться!</h1>
-        </header>
+      <header className={'header ' + this.props.className}>
+        <div className='header__container'>
+          <Title className='header__title'/>
+          <Menu className='header__menu'/>
+        </div>
+      </header>
     );
   }
 }
+
+const Title = ({className}) => (
+  <div className={className}>IT-курсы</div>
+);
+
+const Menu = ({className}) => (
+  <div className={className}>
+    <a className={className + '-link'} href='#'>Курсы</a>
+    <a className={className + '-link'} href='#'>Преподаватели</a>
+    <a className={className + '-link'} href='#'>Мероприятия</a>
+    <Sign className={className + '-sign'}/>
+  </div>
+)
+
+const Sign = ({className}) => (
+  <a className={className} href='#'>Войти</a>
+);
