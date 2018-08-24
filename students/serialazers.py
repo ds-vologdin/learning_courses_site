2ebf979c5ce8122b60a8458526c672d912a82e53
Dmitry Vologdin <ds.vologdin@yandex.ru>
2018-08-24 15:38:46 +0300

    rest создание пользователя

--- students/serialazers.py
+++ students/serialazers.py
@@ -6,4 +6,17 @@ from .models import UserProfile
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
-        fields = ('pk', 'username', 'email', 'is_staff')
+        fields = ('pk', 'username', 'email', 'first_name', 'last_name',
+                  'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = UserProfile(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
