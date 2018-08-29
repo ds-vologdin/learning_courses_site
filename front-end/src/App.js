d9b92de6b13109a8eb5651f28956db3de9de1593
Dmitry Vologdin <ds.vologdin@yandex.ru>
2018-08-29 19:03:07 +0300

    MainPage вынес в отдельный компанент

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,47 +1,21 @@
 import React, { Component } from 'react';
-import './App.less';
-import Header from './Header';
-import NextCourses from './NextCourses';
-import ButtonsMain from './ButtonsMain';
-import Work from './Work';
-import Teachers from './Teachers';
-import Footer from './Footer';
-import ModalSign from './Sign';
+import './less/App.less';
+import MainPage from './MainPage';
+
 
 
 class App extends Component {
   constructor(props) {
       super();
       this.state = {
-          is_active_sign_form: false,
+          page: 'main',
       };
-  }
-  checker_sign_form = () => {
-      this.setState({
-          is_active_sign_form: !this.state.is_active_sign_form
-      });
-  }
-  show_sign_form = () => {
-    this.setState({
-        is_active_sign_form: true,
-    });
-  }
-  hide_sign_form = () => {
-    this.setState({
-        is_active_sign_form: false,
-    });
   }
   render() {
     return (
       <div className="app">
         <div className='app__container'>
-          {this.state.is_active_sign_form && <ModalSign className="app__sign" close={this.hide_sign_form}/>}
-          <Header className="app__header" action={this.show_sign_form}/>
-          <NextCourses className="app__next-courses"/>
-          <ButtonsMain className="app__buttons"/>
-          <Work className="app__work"/>
-          <Teachers className="app__teachers"/>
-          <Footer className="app__footer"/>
+        {this.state.page === 'main' && <MainPage />}
         </div>
       </div>
     );
---
+++ front-end/src/MainPage.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+
+import './less/MainPage.less';
+import Header from './Header';
+import NextCourses from './NextCourses';
+import ButtonsMain from './ButtonsMain';
+import Work from './Work';
+import Teachers from './Teachers';
+import Footer from './Footer';
+import ModalSign from './Sign';
+
+
+class MainPage extends Component {
+  constructor(props) {
+      super();
+      this.state = {
+          is_active_sign_form: false,
+      };
+  }
+  checker_sign_form = () => {
+      this.setState({
+          is_active_sign_form: !this.state.is_active_sign_form
+      });
+  }
+  show_sign_form = () => {
+    this.setState({
+        is_active_sign_form: true,
+    });
+  }
+  hide_sign_form = () => {
+    this.setState({
+        is_active_sign_form: false,
+    });
+  }
+  render() {
+    return (
+      <div className='main app__main'>
+        {this.state.is_active_sign_form && <ModalSign className="app__sign" close={this.hide_sign_form}/>}
+        <Header className="main__header" action={this.show_sign_form}/>
+        <NextCourses className="main__next-courses"/>
+        <ButtonsMain className="main__buttons"/>
+        <Work className="main__work"/>
+        <Teachers className="main__teachers"/>
+        <Footer className="main__footer"/>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
