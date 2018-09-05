11549cf3ada15e23552f8ad6b0170fada0d8def5
Dmitry Vologdin <ds.vologdin@yandex.ru>
2018-09-05 13:11:09 +0300

    удалил старый фронт

--- html/js/main_menu.js
+++
@@ -1,8 +0,0 @@
-// Отображение dropdown меню (jquery)
-$('.dropdown-js-control').hover(
-  function() {
-    $(this).find('.dropdown-js').stop(true, true).delay(200).fadeIn();
-  },
-  function() {
-    $(this).find('.dropdown-js').stop(true, true).delay(200).fadeOut();
-  })
