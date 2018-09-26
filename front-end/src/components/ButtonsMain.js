486097376ea7cee5b10d6ff92978ebeabf8eab60
Dmitry Vologdin <ds.vologdin@yandex.ru>
2018-09-26 10:16:37 +0300

    почистил ButtonsMain

--- front-end/src/components/ButtonsMain.js
+++ front-end/src/components/ButtonsMain.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import PropTypes from "prop-types";
+
 import './less/ButtonsMain.less';
 
 
@@ -8,6 +10,13 @@ const ButtonsMain = ({className}) => (
     <div className='buttons__button buttons__button--dev-curses'>Курсы в разработке</div>
     <div className='buttons__button buttons__button--events'>Мероприятия</div>
   </div>
-)
+);
+
+ButtonsMain.defaultProps = {
+  className: '',
+};
+ButtonsMain.propTypes = {
+  className: PropTypes.string,
+};
 
 export default ButtonsMain;
