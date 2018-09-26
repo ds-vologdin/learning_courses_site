481425bb188b745677a363266c90c99abc987df2
Dmitry Vologdin <ds.vologdin@yandex.ru>
2018-09-26 11:37:13 +0300

    почистил Teachers

--- front-end/src/components/Teachers.js
+++ front-end/src/components/Teachers.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import PropTypes from "prop-types";
+
 import './less/Teachers.less';
 
 
@@ -9,18 +12,38 @@ const Teachers = ({className, teachers}) => (
       {teachers.map(t =>  <Teacher className='teachers__item'name={t.name} course={t.course} image={t.image} key={t.name}/>)}
     </div>
   </div>
-)
+);
+
+Teachers.defaultProps = {
+  className: '',
+  teachers: [],
+};
+Teachers.propTypes = {
+  className: PropTypes.string,
+  teachers: PropTypes.array,
+};
 
 
 const Teacher = ({className, name, course, image}) => {
-  className = 'teacher ' + className;
+  className = 'teacher-item ' + className;
   return (
-    <a className={className} href='/teacher/id/'>
-        <div className='teacher__image' style={{ backgroundImage: `url(${image})` }}/>
-        <div className='teacher__name'>{name}</div>
-        <div className='teacher__course'>{course}</div>
-    </a>
+    <Link className={className} to='/teacher/id/'>
+        <div className='teacher-item__image' style={{ backgroundImage: `url(${image})` }}/>
+        <div className='teacher-item__name'>{name}</div>
+        <div className='teacher-item__course'>{course}</div>
+    </Link>
   )
-}
+};
+
+Teacher.defaultProps = {
+  className: '',
+  name: '',
+  course: '',
+};
+Teacher.propTypes = {
+  className: PropTypes.string,
+  name: PropTypes.string,
+  course: PropTypes.string,
+};
 
 export default Teachers;
