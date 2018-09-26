268da575afe9226ebed4e8bc159776bbd2639502
Dmitry Vologdin <ds.vologdin@yandex.ru>
2018-09-26 10:00:18 +0300

    почистил Header

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -1,38 +1,53 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import {Link} from 'react-router-dom';
+import PropTypes from "prop-types";
 
 import {SHOW_SIGN_FORM_ACTION} from '../actions/sign_form';
 import './less/Header.less';
 
-class Header extends Component {
-  render() {
-    return (
-      <header className={'header ' + this.props.className}>
-        <div className='header__container'>
-          <Link className='header__title' to='/'>IT-курсы</Link>
-          <Menu className='header__menu'
-            action={this.props.show_sign_form}
-            is_authorized={this.props.is_authorized}
-          />
-        </div>
-      </header>
-    );
-  }
-}
 
-const Menu = ({className, action, sign_button, is_authorized}) => (
-  <div className={className}>
-    <Link className={className + '-link'} to='/courses'>Курсы</Link>
-    <Link className={className + '-link'} to='/teachers'>Преподаватели</Link>
-    {is_authorized && <Link className={className + '-sign'} to='/lk'>Личный кабинет</Link>}
-    {!is_authorized && <Sign className={className + '-sign'} action={action} />}
-  </div>
-)
-
-const Sign = ({className, action}) => (
-  <div className={className} onClick={action}>Войти | Зарегистрироваться</div>
+const Header = ({className, show_sign_form, is_authorized}) => (
+  <header className={'header ' + className}>
+    <div className='header__container'>
+      <Link className='header__title' to='/'>IT-курсы</Link>
+      <Menu className='header__menu' action={show_sign_form} is_authorized={is_authorized}/>
+    </div>
+  </header>
 );
+
+Header.defaultProps = {
+  className: '',
+  show_sign_form: () => true,
+  is_authorized: false,
+};
+
+Header.propTypes = {
+  className: PropTypes.string,
+  show_sign_form: PropTypes.func,
+  is_authorized: PropTypes.bool,
+};
+
+
+const Menu = ({action, is_authorized}) => (
+  <div className='header__menu'>
+    <Link className='header__menu-link' to='/courses'>Курсы</Link>
+    <Link className='header__menu-link' to='/teachers'>Преподаватели</Link>
+    {is_authorized && <Link className='header__menu-sign' to='/lk'>Личный кабинет</Link>}
+    {!is_authorized && <div className='header__menu-sign' onClick={action}>Войти | Зарегистрироваться</div>}
+  </div>
+);
+
+Menu.defaultProps = {
+  action: () => true,
+  is_authorized: false,
+};
+
+Menu.propTypes = {
+  action: PropTypes.func,
+  is_authorized: PropTypes.bool,
+};
+
 
 export default connect(
   state => ({
